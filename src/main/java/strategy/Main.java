3f557b8bead5ed4c1449c61ab71ff49e4a4509e9
meraj <merajmusavi1243m@gmail.com>
2024-04-02 13:18:03 +0330

    strategy pattern ! : )

---
+++ src/main/java/strategy/Main.java
@@ -0,0 +1,12 @@
+package strategy;
+
+public class Main {
+    public static void main(String[] args) {
+        ImageStorage imageStorage = new ImageStorage(
+                new JpegCompressor(),new BlackAndWhiteFilter()
+        );
+
+        imageStorage.store("file a ");
+
+    }
+}
